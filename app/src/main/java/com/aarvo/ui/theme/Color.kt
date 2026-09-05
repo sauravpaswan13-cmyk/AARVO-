e2e6bb59470c36f262978d913a6e038fd4d0d120
sauravpaswan13-cmyk <sauravpaswan13@gmail.com>
2026-09-05 16:38:51 +0530

    Step 4: define AARVO color system

---
+++ app/src/main/java/com/aarvo/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.aarvo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AarvoPrimary = Color(0xFF6750A4)
+val AarvoOnPrimary = Color(0xFFFFFFFF)
+val AarvoPrimaryContainer = Color(0xFFEADDFF)
+val AarvoOnPrimaryContainer = Color(0xFF21005D)
+val AarvoSecondary = Color(0xFF625B71)
+val AarvoBackground = Color(0xFFFFFBFE)
+val AarvoSurface = Color(0xFFFFFBFE)
+
+val AarvoPrimaryDark = Color(0xFFD0BCFF)
+val AarvoOnPrimaryDark = Color(0xFF381E72)
+val AarvoPrimaryContainerDark = Color(0xFF4F378B)
+val AarvoOnPrimaryContainerDark = Color(0xFFEADDFF)
+val AarvoSecondaryDark = Color(0xFFCCC2DC)
