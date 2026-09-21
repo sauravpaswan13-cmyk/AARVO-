9ae3fb41c9931e4fe0a5182615b7970ba92bc8d7
sauravpaswan13-cmyk <sauravpaswan13@gmail.com>
2026-09-21 13:08:43 +0530

    fix: make guest entry use a clean MainActivity task

--- app/src/main/java/com/aarvo/WelcomeActivity.kt
+++ app/src/main/java/com/aarvo/WelcomeActivity.kt
@@ -116,7 +116,7 @@ class WelcomeActivity : ComponentActivity() {
             .putString("user_role", "BUYER")
             .remove("auth_token")
             .apply()
-        startActivity(Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+        startActivity(Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
         finish()
     }
 
