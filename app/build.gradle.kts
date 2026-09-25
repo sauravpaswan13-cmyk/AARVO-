20cfbeea2682957494ad3d713de7b4609b3666d7
sauravpaswan13-cmyk <sauravpaswan13@gmail.com>
2026-09-25 10:11:15 +0530

    Fix Android release resource shrinking build failure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         getByName("release") {
             if (hasReleaseSigning) signingConfig = signingConfigs.getByName("production")
             isMinifyEnabled = false
-            isShrinkResources = true
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
